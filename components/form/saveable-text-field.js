e6360193d5c308e5543ec992203b252f7422440d
Chris Esplin <chris@christopheresplin.com>
2018-08-09 09:30:49 -0600

    Repairing saveable text field

--- components/form/saveable-text-field.js
+++ components/form/saveable-text-field.js
@@ -9,7 +9,8 @@ export default class SaveableTextField extends React.Component {
     super();
 
     this.state = {
-      proposedValue: '',
+      incomingValue: '',
+      currentValue: '',
     };
   }
 
@@ -18,28 +19,47 @@ export default class SaveableTextField extends React.Component {
   }
 
   get isDirty() {
-    return this.value != this.state.proposedValue;
+    return this.value != this.state.currentValue;
+  }
+
+  componentDidMount() {
+    this.compareIncomingValue(null, this.props.value);
+  }
+
+  componentDidUpdate(prevProps) {
+    this.compareIncomingValue(prevProps.value, this.props.value);
+  }
+
+  compareIncomingValue(prev, current) {
+    if (prev != current) {
+      this.setState({ currentValue: current });
+    }
   }
 
   handleChange({ target: { value } }) {
-    this.setState({ proposedValue: value });
+    this.setState({ currentValue: value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
     e.stopPropagation();
 
-    this.props.onSave(this.state.proposedValue);
+    this.props.onSave(this.state.currentValue);
   }
 
   render() {
-    const propsExceptOnSave = { ...this.props };
+    const filteredProps = { ...this.props };
 
-    delete propsExceptOnSave.onSave;
+    delete filteredProps.onSave;
+    delete filteredProps.value;
 
     return (
       <form onSubmit={this.handleSubmit.bind(this)}>
-        <TextField {...propsExceptOnSave} onChange={this.handleChange.bind(this)}>
+        <TextField
+          {...filteredProps}
+          value={this.state.currentValue}
+          onChange={this.handleChange.bind(this)}
+        >
           <Button raised disabled={!this.isDirty} onClick={console.log}>
             {this.props.children || 'Save'}
           </Button>
